b41d6605386961559d50da4cd80e21d65888f61e
Hector Rios <hrios@sugarcrm.com>
2020-02-11 10:09:51 +0100

    rename default Test class and enable phpunit XML

---
+++ tests/SugarModulePackagerTest.php
@@ -0,0 +1,12 @@
+<?php
+//declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class SugarModulePackager extends TestCase
+{
+    public function testSomeCase()
+    {
+        $this->assertTrue(false);
+    }
+}
