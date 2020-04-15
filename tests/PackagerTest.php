c8f4006e226af64974e0427d7c074c20fdf6d170
Hector Rios <hrios@sugarcrm.com>
2020-04-15 23:46:21 +0200

    remove dummy sayMessage test case and add a simple test case to see what happens when you call build without a version

--- tests/PackagerTest.php
+++ tests/PackagerTest.php
@@ -2,19 +2,31 @@
 
 namespace SugarModulePackager\Test;
 
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
 use PHPUnit\Framework\TestCase;
+use SugarModulePackager\EchoMessageOutputter;
+use SugarModulePackager\FileReaderWriter;
+use SugarModulePackager\Packager;
 
 class PackagerTest extends TestCase
 {
-    public function testSayMessage()
-    {
-        $startMessage = "Hello";
-        $expectedMessage = "say Hello" . PHP_EOL;
-        $packager = new \SugarModulePackager\Packager();
-        $this->assertEquals($expectedMessage,
-            $packager->sayMessage($startMessage));
-        $this->assertTrue(true);
+    /* @var vfsStreamDirectory */
+    private $rootDir;
 
+    public function setup()
+    {
+        $this->rootDir = vfsStream::setup('exampleDir');
+    }
+
+    public function testBuildWithNoVersionProvided()
+    {
+        $expectedMessage = 'Provide version number' . PHP_EOL;
+        $messageOutputter = new EchoMessageOutputter();
+        $packager = new Packager(new FileReaderWriter(), $messageOutputter);
+
+        $packager->build();
+        $this->assertEquals($expectedMessage, $messageOutputter->getLastMessage());
     }
 
 }
