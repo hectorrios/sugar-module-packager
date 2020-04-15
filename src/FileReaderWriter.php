86bb87431e0f74e4e61a767cee0b2b00784636a9
Hector Rios <hrios@sugarcrm.com>
2020-04-15 23:44:08 +0200

    add a constructor parameter to take in a base directory for all other files or directories that might be created or retrieved

--- src/FileReaderWriter.php
+++ src/FileReaderWriter.php
@@ -7,26 +7,38 @@ namespace SugarModulePackager;
 class FileReaderWriter
 {
 
+    /* @var string */
+    private $baseDirectory;
+
     /**
      * FileReaderWriter constructor.
+     * @param string $baseDirectory
      */
-    public function __construct()
+    public function __construct($baseDirectory = '')
     {
+        $this->baseDirectory = $baseDirectory;
     }
 
     public function writeFile($filename = '', $content = '')
     {
         if (!empty($filename)) {
+            $filename = $this->constructPathWithBase($filename);
             file_put_contents($filename, $content);
         }
     }
 
     public function readFile($filename = '')
     {
-        if (!empty($filename) && file_exists($filename)) {
-            return file_get_contents($filename);
+        if (empty($filename)) {
+            return '';
         }
-        return '';
+
+        $filename = $this->constructPathWithBase($filename);
+        if (!file_exists($filename)) {
+           return '';
+        }
+
+        return file_get_contents($filename);
     }
 
     public function copyFile($src, $dst)
@@ -39,10 +51,28 @@ class FileReaderWriter
     public function createDirectory($directory = '')
     {
         if (!empty($directory)) {
+            //construct the dir taking baseDirectory into account
+            $directory = $this->constructPathWithBase($directory);
             if (!is_dir($directory)) {
                 mkdir($directory, 0777, true);
             }
         }
+    }
+
+    /**
+     * @param string $fragment The directory or file to add to the base directory
+     * @return string Full-path with the baseDirectory
+     */
+    private function constructPathWithBase($fragment)
+    {
+        $fullPath = '';
+        if (empty($this->baseDirectory)) {
+            $fullPath = $fragment;
+        } else {
+            $fullPath = $this->baseDirectory . DIRECTORY_SEPARATOR . $fragment;
+        }
+
+        return $fullPath;
     }
 
 }
