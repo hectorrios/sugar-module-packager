dbc5f03a5b2070e265951f57880312f10b2ba3f5
Hector Rios <hrios@sugarcrm.com>
2020-04-23 10:46:34 +0200

    remove the additional methods and variables introduced to help with testing

--- src/EchoMessageOutputter.php
+++ src/EchoMessageOutputter.php
@@ -6,25 +6,10 @@ namespace SugarModulePackager;
 
 class EchoMessageOutputter implements MessageOutputter
 {
-    /* @var string $lastMessage */
-    private $lastMessage;
-
     public function message($out = '')
     {
-        if (empty($out)) {
-            //reset lastMessage to blank
-            $this->lastMessage = '';
-            return;
-        }
-
         $formattedMessage = $out . PHP_EOL;
-        $this->lastMessage = $formattedMessage;
         echo $formattedMessage;
-    }
-
-    public function getLastMessage()
-    {
-        return $this->lastMessage;
     }
 
 }
