6512ae141bc27a3930fad263b16ba636809ba996
Hector Rios <hrios@sugarcrm.com>
2020-05-03 23:05:03 +0200

    add method comments to the copyDirectory method

--- src/ReaderWriter.php
+++ src/ReaderWriter.php
@@ -25,6 +25,9 @@ interface ReaderWriter
     public function resolvePath($path = '');
 
     /**
+     * Copies the contents of the source directory, including sub-directories, over to the
+     * destination directory. Any files passed in via the $filesToExclude variadic parameter will
+     * be excluded from the copy over to the destination directory.
      * @param string $srcDir
      * @param string $destDir
      * @param string ...$filesToExclude
